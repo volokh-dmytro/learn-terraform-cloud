4865063052ceb5187681b5be42e8ac1de12dcf4c
Dmytro Volokh <volokh.dima1995@gmail.com>
2023-09-22 21:42:36 +0300

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,17 +1,17 @@
 terraform {
 
-  cloud {
-    organization = "organization-name"
+  # cloud {
+  #   organization = "example-org-83ce38"
 
-    workspaces {
-      name = "learn-terraform-cloud"
-    }
-  }
+  #   workspaces {
+  #     name = "learn-terraform-cloud"
+  #   }
+  # }
 
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.28.0"
+      version = "~> 3.74.2"
     }
   }
 
